a548b1d8e706053091e956271f5ed6d2e17fce64
Jeffery Marfo <marfojeffery31@gmail.com>
2025-04-17 10:55:17 +0000

    CheckIn system complete

--- src/pages/user/Landing.jsx
+++ src/pages/user/Landing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../../components/HeroSection";
+import PatientCheckInSystem from "../../components/Hospital";
 
 const Landing = () => {
   return (
     <div>
       <HeroSection />
+      <PatientCheckInSystem />
       Landing goes here
     </div>
   );
