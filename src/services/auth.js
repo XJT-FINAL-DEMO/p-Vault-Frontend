4868d64d45ec47861993c9aa2b02908626daf28a
Jeffery Marfo <marfojeffery31@gmail.com>
2025-04-18 21:22:49 +0000

    integration for login and sign up working

--- src/services/auth.js
+++ src/services/auth.js
@@ -17,20 +17,16 @@
 //     });
 // };
 
-import { apiClient } from "./config";
+import API from "./config";
 
-export const apiSignup = async (payload) => {
-    return apiClient.post("/users/signup", payload, {
-        headers: { // Fix: lowercase 'headers' (not 'Headers')
-            "Content-Type": "application/json",
-        },
-    });
+
+export const signupUser = async (formData) => {
+  const response = await API.post("/users/signup", formData);
+  return response.data; 
 };
 
-export const apiLogin = async (payload) => {
-    return apiClient.post("/users/login", payload, {
-        headers: { // Fix: lowercase 'headers' (not 'Headers')
-            "Content-Type": "application/json"
-        },
-    });
+
+export const loginUser = async (formData) => {
+  const response = await API.post("/users/login", formData);
+  return response.data; 
 };
